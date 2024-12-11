382c0c37e8be6b6425bffc793862acae6c11c98f
fmattaperdomo <fmattaperdomo@gmail.com>
2024-12-11 16:01:18 -0500

    exceptions added

--- src/main/java/com/fmattaperdomo/inventoryapp/controllers/ProductController.java
+++ src/main/java/com/fmattaperdomo/inventoryapp/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.fmattaperdomo.inventoryapp.controllers;
 
+import com.fmattaperdomo.inventoryapp.exceptions.ResourceNotFound;
 import com.fmattaperdomo.inventoryapp.models.Product;
 import com.fmattaperdomo.inventoryapp.services.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -32,6 +34,16 @@ public class ProductController {
     public Product addProduct(@RequestBody Product product){
         logger.info("Add Product : " + product);
         return this.productService.saveProduct(product);
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(
+            @PathVariable int id){
+        Product product =
+                this.productService.searchProductById(id);
+        if(product != null)
+            return ResponseEntity.ok(product);
+        else
+            throw new ResourceNotFound("The Id was not found: " + id);
     }
 
 }
